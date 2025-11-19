5476a3b0100d093e7b98d9de380b71244922746d
Saeed-Lababidi <69202298+Saeed-Lababidi@users.noreply.github.com>
2025-11-19 22:37:18 +0300

    Added Renewal Auto-Calculation Logic in the Subscription Model with validation of renewal date being in the future

--- aajil-subscriptions/subscriptions/admin.py
+++ aajil-subscriptions/subscriptions/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Subscription
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'cost', 'billing_cycle', 'start_date', 'renewal_date', 'is_active', 'category')
+    list_filter = ('billing_cycle', 'is_active', 'category')
+    search_fields = ('name', 'category')
--- aajil-subscriptions/subscriptions/models.py
+++ aajil-subscriptions/subscriptions/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+from dateutil.relativedelta import relativedelta
 
 # Model details and validation prompted from chatgpt
 class Subscription(models.Model):
@@ -11,9 +13,43 @@ class Subscription(models.Model):
     cost = models.DecimalField(max_digits=10, decimal_places=2)
     billing_cycle = models.CharField(max_length=10, choices=BILLING_CYCLE_CHOICES)
     start_date = models.DateField()
-    renewal_date = models.DateField()
+    renewal_date = models.DateField(blank=True, null=True)
     is_active = models.BooleanField(default=True)
     category = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
         return f"{self.name} ({self.billing_cycle})"
+
+    def calculate_next_renewal(self):
+        """
+        Calculate the next renewal date based on start_date, billing_cycle,
+        and today's date. The result should always be in the future.
+        """
+        if not self.start_date:
+            return None
+
+        if self.billing_cycle == 'monthly':
+            delta = relativedelta(months=1)
+        else:
+            delta = relativedelta(years=1)
+
+        current = self.start_date
+
+        # Always move forward until renewal is in the future
+        today = timezone.localdate()
+        while current <= today:
+            current = current + delta
+
+        return current
+
+    def save(self, *args, **kwargs):
+        """
+        Override save so that renewal_date is automatically calculated whenever the subscription is saved.
+        If there is no renewal_date yet OR start_date/billing_cycle changed, we recalculate.
+        This also helps in validating renewal date not being in the past.
+        """
+        calculated = self.calculate_next_renewal()
+        if calculated is not None:
+            self.renewal_date = calculated
+
+        super().save(*args, **kwargs)
